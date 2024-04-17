c36fb377f7f22364311d3f267a94ef5b905ac5cc
sikandar <sikandarmiirza@gmail.com>
2024-04-17 12:29:37 +0500

    cors issue resolved

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const { PORT, mongoDBURL } = require("./models/config.js");
-const { checkConnection } = require("./models/connection.js");
+const express = require("express")
+const session = require('express-session')
+const morgan = require("morgan")
+const cors = require('cors')
+const { PORT, mongoDBURL } = require("./models/config.js")
+const { checkConnection } = require("./models/connection.js")
 const personRouter = require("./routes/personRoute.js")
 const postRouter = require("./routes/postRoute.js")
 
@@ -9,6 +12,9 @@ const app = express();
 
 //---- Middleware for parsing request body ---- 
 app.use(express.json());
+app.use(morgan('dev'))
+app.use(cors());
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 
 
 app.use('/person', personRouter);
